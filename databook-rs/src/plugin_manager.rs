9398d5f0394c0177956e0d3b23cfb2b21386aeee
era <me@elias.sh>
2022-07-25 10:44:10 +0100

    adds plugin manager and cli options

---
+++ databook-rs/src/plugin_manager.rs
@@ -0,0 +1,4 @@
+struct PlugManager {
+    // any plugin (wasm files) in this folder will be registered
+    folder: std::path::Path,
+}
--- databook-rs/src/server.rs
+++ databook-rs/src/server.rs
@@ -1,12 +1,25 @@
+use clap::Parser;
 use databook::databook_server::{Databook, DatabookServer};
 use databook::{GetRequest, GetResponse};
-
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 use tracing::instrument;
 
+mod plugin_manager;
+
 pub mod databook {
     tonic::include_proto!("databook");
+}
+
+// CLI arguments to start the server
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    // Path of the plugin folder so we know from where to load it
+    #[clap(short, long, value_parser, default_value_t = String::from("./plugins"))]
+    plugin_folder: String,
+    #[clap(short, long, value_parser, default_value_t = String::from("[::1]:50051"))]
+    address_to_listen: String,
 }
 
 #[derive(Debug, Default)]
@@ -24,11 +37,12 @@ impl Databook for DatabookGrpc {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
     // Setups tracing
     let subscriber = tracing_subscriber::FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber)?;
     // Setups GRPC server
-    let addr = "[::1]:50051".parse()?;
+    let addr = args.address_to_listen.parse()?;
     Server::builder()
         .add_service(DatabookServer::new(DatabookGrpc::default()))
         .serve(addr)
