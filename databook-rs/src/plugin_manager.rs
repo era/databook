c2b6acbbe1c076091ea6c51015dbf500d04340a5
era <me@elias.sh>
2022-07-25 10:54:38 +0100

    adding plugins hashmap

--- databook-rs/src/plugin_manager.rs
+++ databook-rs/src/plugin_manager.rs
@@ -1,4 +1,24 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Plugin {
+    plugin_type: String,
+}
+
+#[derive(Debug)]
 struct PlugManager {
     // any plugin (wasm files) in this folder will be registered
-    folder: std::path::Path,
+    folder: Box<std::path::Path>,
+
+    //all plugins registered, <Type, Plugin>
+    plugins: HashMap<String, Plugin>,
+}
+
+impl PlugManager {
+    pub fn new(folder: Box<std::path::Path>) -> Self {
+        return PlugManager {
+            folder,
+            plugins: HashMap::new(),
+        };
+    }
 }
