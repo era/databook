9dfe468ee9667c61cc9d2d5be050e9ae905c9eb4
era <me@elias.sh>
2022-10-04 09:03:04 +0100

    adding tests for http mod

--- databook-rs/src/http.rs
+++ databook-rs/src/http.rs
@@ -28,20 +28,60 @@ pub fn http_headers_from_str(
 
 pub fn http_headers_to_str(header_map: HeaderMap) -> String {
     let mut header_as_str = Vec::<String>::new();
+    let mut headers = Vec::<String>::new();
     for (key, value) in header_map {
         header_as_str.push(key.unwrap().as_str().into()); //TODO
         header_as_str.push(value.to_str().unwrap().into()); //TODO
+        headers.push(header_as_str.join("="));
+        header_as_str.clear();
     }
-    header_as_str.join("&")
+    headers.join("&")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use hyper::http::request::Builder;
 
     #[test]
     fn test_build_http_url() {
         let url = build_http_url("http://www.elias.sh/", "ab=1&aa=2");
         assert_eq!(url, "http://www.elias.sh/?ab=1&aa=2");
+    }
+
+    #[test]
+    fn test_http_headers_from_str() {
+        let header = "content=x&something=y";
+        let mut headerMap = HeaderMap::new();
+        headerMap.insert(
+            "content".to_string().parse::<HeaderName>().unwrap(),
+            "x".to_string().parse::<HeaderValue>().unwrap(),
+        );
+        headerMap.insert(
+            "something".to_string().parse::<HeaderName>().unwrap(),
+            "y".to_string().parse::<HeaderValue>().unwrap(),
+        );
+
+        assert_eq!(
+            &headerMap,
+            http_headers_from_str(header, Builder::new())
+                .headers_ref()
+                .unwrap()
+        )
+    }
+
+    #[test]
+    fn test_http_headers_to_str() {
+        let mut header_map = HeaderMap::new();
+        header_map.insert(
+            "content".to_string().parse::<HeaderName>().unwrap(),
+            "x".to_string().parse::<HeaderValue>().unwrap(),
+        );
+        header_map.insert(
+            "something".to_string().parse::<HeaderName>().unwrap(),
+            "y".to_string().parse::<HeaderValue>().unwrap(),
+        );
+
+        assert_eq!("content=x&something=y", http_headers_to_str(header_map))
     }
 }
